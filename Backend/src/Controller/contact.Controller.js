74eb3aec171fb9f106eb22db2bf094f5a63904e0
nileshnaik06 <pnaik1196@gmail.com>
2026-03-03 23:10:23 +0530

    minor changes

--- Backend/src/Controller/contact.Controller.js
+++ Backend/src/Controller/contact.Controller.js
@@ -1,6 +1,7 @@
 const { Resend } = require("resend");
 
 const resend = new Resend(process.env.RESEND_API_KEY);
+console.log(process.env.RESEND_API_KEY)
 
 async function sendMail(req, res) {
   try {
